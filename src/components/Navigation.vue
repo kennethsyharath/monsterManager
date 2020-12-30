d1ff1389ce9325a49ff8475fccb59cd1354730a5
Kenneth Syharath <kenneth.syharath@gmail.com>
2020-12-30 18:29:41 -0500

    Editor styling setup exploration
    Initial editor styling setup. I should move this to its own branch soon. Like next actually.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-app-bar color="primary" dark>
+      <v-spacer/>
+      <router-link class="router-text" to="/">Home</router-link>
+      <v-spacer/>
+      <router-link class="router-text" to="/about">About</router-link>
+      <v-spacer/>
+      <router-link class="router-text" to="/editor">Editor</router-link>
+      <v-spacer/>
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Navigation extends Vue {}
+</script>
+
+<style scoped>
+    .router-text {
+        color: black;
+    }
+</style>
---
+++ src/views/Editor.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="editor">
+    <v-container class="editor-container">
+        <v-row no-gutters>
+            <v-col
+                v-for="n in 6"
+                :key="n"
+                cols="12"
+                sm="6"
+            >
+                <v-card
+                    class="pa-2"
+                    outlined
+                    tile
+                >
+                    One of three columns
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+    components: {
+        
+    }
+})
+
+export default class Editor extends Vue {}
+</script>
